0a3d98c1aab96a65e91487f2ae37516058d6eb3e
igorovic <hello@dyve.dev>
2021-08-09 08:39:18 +0200

    initial

---
+++ dist/main.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const TRouter = require("trouter");
+//@ts-ignore
+const R = new TRouter();
+console.debug(R);
---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import * as TRouter from "trouter";
+//@ts-ignore
+const R = new TRouter();
+
+console.debug(R);
